a7745332a7be1e4843ec5f93fc87efc64fdb7ced
binaryfunda <binaryfundaforweb@gmail.com>
2020-06-28 21:17:34 +0530

    forEach Example

---
+++ src/org/foreachjava8/main/ForEachLoopExampleJava8.java
@@ -0,0 +1,79 @@
+package org.foreachjava8.main;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.foreachjava8.model.Student;
+
+public class ForEachLoopExampleJava8 {
+
+	public static void main(String args[]) {
+		
+		
+		
+		
+
+		
+		List<Student> studentList = new ArrayList<>();
+		studentList.add(new Student(1, "RAJESH"));
+		studentList.add(new Student(1, "SHAM"));
+		studentList.add(new Student(1, "ANDY"));
+		studentList.add(new Student(1, "ALI"));
+		
+		
+		// @@@ Iteration Consumer Interface example@@@
+		Consumer<Student> printStudent = new Consumer<Student>() {
+		    public void accept(Student student) {
+		        System.out.println(student.getName());
+		    };
+		};
+		studentList.forEach(printStudent);
+		// --------------------------------------------
+		
+		
+		// @@@ Iteration  Method Reference example@@@
+		studentList.forEach(System.out::println);
+		// --------------------------------------------
+		
+		// @@@ Iteration over List example@@@
+
+		studentList.forEach(student -> {
+			System.out.println(student.getName());
+		});
+		// --------------------------------------------
+		
+
+		// @@@ Iteration over Set example@@@
+		
+		Set<String> uniquesurName = new HashSet<>(Arrays.asList("RAM", "SAM", "CAN", "ABHISHEK"));
+		uniquesurName.forEach(System.out::println);
+		// --------------------------------------------
+		
+		
+
+		// @@@ Iteration over Map example@@@
+		
+		Map<Integer, String> studentNamesMap = new HashMap<>();
+		studentNamesMap.put(1, "RAHUL");
+		studentNamesMap.put(2, "AMAN");
+		studentNamesMap.put(3, "REENA");
+		studentNamesMap.forEach((key, value) -> System.out.println(key + " " + value));
+		// --------------------------------------------
+		
+
+		// @@@ Iteration over Map example@@@
+		Queue<String> namesQueue = new ArrayDeque<>(Arrays.asList("RAM", "SAM", "ABHISHEK"));
+		namesQueue.forEach(System.out::println);
+		// --------------------------------------------
+
+	}
+
+}
---
+++ src/org/foreachjava8/model/Student.java
@@ -0,0 +1,31 @@
+package org.foreachjava8.model;
+
+public class Student {
+	int roleNumber;
+	String name;
+	
+	public Student(int roleNumber,String name){
+		this.roleNumber=roleNumber;
+		this.name=name;
+	}
+	
+	public int getRoleNumber() {
+		return roleNumber;
+	}
+	public void setRoleNumber(int roleNumber) {
+		this.roleNumber = roleNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+
+}
